df8541a5cea7d72a329e11bf25905f2af3d69689
Leonardo Bittencourt Otero <85591297+leonardo-otero390@users.noreply.github.com>
2021-11-25 14:44:17 -0400

    fix: http=>httpś axiosBase

--- src/services/axiosBase.js
+++ src/services/axiosBase.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosBase = axios.create({
-	baseURL: "http://grati-box-driven.herokuapp.com",
+	baseURL: "https://grati-box-driven.herokuapp.com",
 });
 
 export default axiosBase;
